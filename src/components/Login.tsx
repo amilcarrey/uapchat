193f0e5ab6e494c1b15d2f1a3d036407962e1712
amilcarrey <amilcarrey.ar@gmail.com>
2022-06-08 16:45:18 -0300

    Test

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,6 +9,7 @@ interface Props {
 export default function Login({ email, password, setEmail, setPassword, login }: Props) {
   return (
     <div className="container mx-auto flex flex-col">
+      <h1>Esto es un cambio</h1>
       <input className="border-2 border-stone-500 rounded-lg px-2 my-2" placeholder="Email" type="email" value={email} onChange={e => setEmail(e.target.value)} />
       <input className="border-2 border-stone-500 rounded-lg px-2 my-2" placeholder="Contraseña" type="password" value={password} onChange={e => setPassword(e.target.value)} />
       <button
